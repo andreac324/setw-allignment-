b1c4614c8c3d1c26be322aa58936c27c73c2ed9f
andreac324 <terisa32873@gmail.com>
2024-09-15 16:26:39 -0700

    Initial Commit

---
+++ ca4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ca4
+//
+//  Created by Andrea d Cazares on 9/15/24.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    cout << left << setw(10) << "course" << right << setw(10) << "students\n";
+    cout << left << setw(10) << "C++ " << right<<setw(10) << "100 \n";
+    cout << left << setw(10) << "Java" << right << setw(7)<< "50" << endl;
+
+
+    return 0;
+}
